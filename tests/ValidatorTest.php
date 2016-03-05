1c6c30a6911f2d61bf90ef840070c00b1bc59e65
msarca <marius.sarca@gmail.com>
2016-03-05 11:50:37 +0200

    Added support for unit testing

---
+++ tests/ValidatorTest.php
@@ -0,0 +1,18 @@
+<?php
+use Opis\Utils\Validator;
+
+class ValidatorTest extends PHPUnit_Framework_TestCase
+{
+
+    protected $validator;
+
+    public function setUp()
+    {
+        $this->validator = new Validator();
+    }
+
+    public function testBasicValidation1()
+    {
+        $this->assertEquals('foo', $this->validator->validate('Field', 'foo'));
+    }
+}
