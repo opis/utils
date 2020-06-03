5568dc18d02ff4a2edd4eaaaead8904ac3db2da7
Marius Sarca <marius.sarca@gmail.com>
2020-06-03 16:51:57 +0300

    Updated

--- src/Placeholder.php
+++ src/Placeholder.php
@@ -1,9 +1,6 @@
 <?php
 /* ===========================================================================
- * Opis Project
- * http://opis.io
- * ===========================================================================
- * Copyright 2015-2016 Marius Sarca
+ * Copyright 2018-2020 Zindex Software
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
