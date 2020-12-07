06b0cd852bab084be7978917e4e6c7218d236897
Sorin Sarca <sarca_sorin@hotmail.com>
2020-12-07 03:03:18 +0200

    Fixed regex

--- src/Uri.php
+++ src/Uri.php
@@ -23,7 +23,7 @@ class Uri
 {
     protected const URI_REGEX = '`^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?$`';
 
-    protected const SCHEME_REGEX = '`^[a-z][a-z0-9+-.]*$`i';
+    protected const SCHEME_REGEX = '`^[a-z][a-z0-9-+.]*$`i';
 
     protected const USER_OR_PASS_REGEX = '`^(?:(?:%[a-f0-9]{2})+|[a-z0-9-._~!$&\'()*+,:;=]+)*$`i';
 
